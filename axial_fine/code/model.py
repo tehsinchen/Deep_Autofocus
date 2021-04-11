630942a45c77a52fba8875789ca347468341c408
Te-Hsin Chen <tp62k6vup@gmail.com>
2021-04-11 17:16:49 +0800

    Add files via upload

---
+++ axial_fine/code/model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model, Sequential
+import efficientnet
+
+
+def model_axial_fine(input_shape, category):
+    model = efficientnet.EfficientNetB2(
+        include_top=True,
+        weights=None,
+        pooling='avg',
+        input_shape=input_shape,
+        classes=category,
+        classifier_activation='sigmoid'
+    )
+    model.summary()
+    return model
+
+
+if __name__ == '__main__':
+    model_axial_fine((256, 256, 7), 7)
+
